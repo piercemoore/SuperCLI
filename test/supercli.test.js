93d70638bf14500f7bac57777beae6bc317d3035
Pierce Moore <pierce@piercemoore.com>
2013-02-23 02:21:27 -0600

    Enabled custom configuration of logging, output, and app

--- test/supercli.test.js
+++ test/supercli.test.js
@@ -4,6 +4,7 @@ scli.log("Running SuperCLI Test Suite!");
 
 scli("This is an SCLI call without a specific function!");
 
+scli.config.hideName();
 scli.log("I am a sample log message!");
 scli.log("I am one of multiple log messages!", "I am another of multiple log messages!");
 
@@ -16,5 +17,10 @@ scli.warn("I am one of multiple warn messages!", "I am another of multiple warn 
 scli.ok("I am a sample ok message!");
 scli.ok("I am one of multiple ok messages!", "I am another of multiple ok messages!");
 
+scli.config.appName("Bananas");
+scli.config.showName();
 scli.success("I am a sample success message!");
 scli.success("I am one of multiple success messages!", "I am another of multiple success messages!");
+
+scli.put("Application Error","error", "App error 1", "App error 2", { error : { name : "App 3" } });
+scli.put("App error 1","log", "App error 2", { error : { name : "App 3" } });
